6acbbcc3696eab7c1627dc2b805199a8a415c365
Your Name <abhilashpandey8170@gmail.com>
2020-12-10 21:27:54 +0530

    list created

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -1,9 +1,17 @@
 import React from "react";
+const ListComponent = (props) => {
 
-const ListComponent = () => {
+console.log('abcd',props.user_data.set)
+const get_list=()=>{
+const list_item =props.user_data.set.map((el)=>{return<li>{el.key}:{el.value}</li>
+ })
+return list_item;
+}
   return (
     <div>
-      <text>Search Component</text>
+      <text>List Component</text>
+      <h4>User</h4>
+      <ul>{get_list()}</ul>
     </div>
   );
 };
